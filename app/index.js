3e5c54027773a71134d77e84b4e3d6c751fd7a28
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-03-14 11:15:04 +0000

    Test function

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
-const { saveEvent } = require('./event')
-const { sendMessage } = require('./signalr')
-const { validateEvent } = require('./event-schema')
+// const { saveEvent } = require('./event')
+// const { sendMessage } = require('./signalr')
+// const { validateEvent } = require('./event-schema')
 
 module.exports = async function (context, message) {
   const event = message
   console.log(`Received event: ${JSON.stringify(event)}!!!!!!!!!!!`)
-  try {
-    if (validateEvent(event)) {
-      await saveEvent(context, event)
-      sendMessage(context, event)
-      context.done()
-    }
-  } catch (error) {
-    context.log.error('Unable to process message:', error)
-  }
+  // try {
+  //   if (validateEvent(event)) {
+  //     await saveEvent(context, event)
+  //     sendMessage(context, event)
+  //     context.done()
+  //   }
+  // } catch (error) {
+  //   context.log.error('Unable to process message:', error)
+  // }
 }
