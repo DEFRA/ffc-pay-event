acc003a7fe736873f4742f2cca881102e2c77ecb
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-03-31 16:36:41 +0100

    Update projection output

--- ffc-pay-event/projection.js
+++ ffc-pay-event/projection.js
@@ -10,8 +10,10 @@ const checkCreateProjection = (context, raisedEvent) => {
 
 const saveProjection = (context, event) => {
   const frn = event?.action?.data?.paymentRequest?.frn ?? event?.action?.data?.frn
-  context.log.info(`Creating projection for ${event.id} with frn ${frn}`)
-  context.bindings.outputSbTopic = { id: event.id, frn }
+  const paymentRequestNumber = event?.action?.data?.paymentRequest?.paymentRequestNumber ?? event?.action?.data?.paymentRequestNumber
+  const agreementNumber = event?.action?.data?.paymentRequest?.agreementNumber ?? event?.action?.data?.agreementNumber
+  context.log.info(`Creating projection for ${event.id} with frn ${frn}, paymentRequestNumber ${paymentRequestNumber}, agreementNumber ${agreementNumber}`)
+  context.bindings.outputSbTopic = { id: event.id, frn, paymentRequestNumber, agreementNumber }
 }
 
 module.exports = { checkCreateProjection }
