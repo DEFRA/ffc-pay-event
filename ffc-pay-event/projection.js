ee621809e5d103f7903a52c827951496e0ddc014
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-04-04 16:16:27 +0100

    Refactor after send events (#10)

--- ffc-pay-event/projection.js
+++ ffc-pay-event/projection.js
@@ -1,6 +1,9 @@
 const checkCreateProjection = (context, raisedEvent) => {
   let createProjection = false
-  createProjection = raisedEvent?.action?.type === 'submission' || raisedEvent?.action?.data?.error?.length > 0
+  createProjection = raisedEvent?.action?.type === 'submission' ||
+    raisedEvent?.action?.type === 'return' ||
+    raisedEvent?.action?.data?.error?.length > 0
+
   if (createProjection) {
     saveProjection(context, raisedEvent)
   }
@@ -9,8 +12,11 @@ const checkCreateProjection = (context, raisedEvent) => {
 }
 
 const saveProjection = (context, event) => {
-  context.log.info(`Creating projection for ${event.id}`)
-  context.bindings.outputSbTopic = { id: event.id }
+  const frn = event?.action?.data?.paymentRequest?.frn ?? event?.action?.data?.frn
+  const paymentRequestNumber = event?.action?.data?.paymentRequest?.paymentRequestNumber ?? event?.action?.data?.paymentRequestNumber
+  const agreementNumber = event?.action?.data?.paymentRequest?.agreementNumber ?? event?.action?.data?.agreementNumber
+  context.log.info(`Creating projection for ${event.id} with frn ${frn}, paymentRequestNumber ${paymentRequestNumber}, agreementNumber ${agreementNumber}`)
+  context.bindings.outputSbTopic = { id: event.id, frn, paymentRequestNumber, agreementNumber }
 }
 
 module.exports = { checkCreateProjection }
