4665c2a2138e8437619abd8f4cf63ff7ef3e06b1
Marc Templeton <marcte@kainos.com>
2022-04-12 10:03:42 +0100

    parsing env string to boolean for signalR check

--- ffc-pay-event/signalr.js
+++ ffc-pay-event/signalr.js
@@ -1,5 +1,5 @@
 const sendMessageToSignalR = (context, message) => {
-  if (process.env.USE_SIGNALR) {
+  if (JSON.parse(process.env.USE_SIGNALR)) {
     context.log.info('signalR event sent')
     context.bindings.signalRMessages = [{
       target: 'payEvent',
